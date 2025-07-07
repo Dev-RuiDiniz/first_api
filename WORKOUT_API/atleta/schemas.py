ccfe0fd416eec683231c12db72a91f12018d6f5c
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-07 15:22:50 -0300

    schemas

---
+++ WORKOUT_API/atleta/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+
+class Atleta(BaseModel):
+    nome: str = Field(description='Nome do Atleta', max_length=100)
+    cpf:  str = Field(description='CPF do Atleta', max_length=11, min_length=11)
+    idade: int = Field(description='Idade do Atleta', ge=0, le=120)
+    peso: float = Field(description='Peso do Atleta', ge=0.0)
+    altura: float = Field(description='Altura do Atleta', ge=0.0)
+    sexo: str = Field(description='Sexo do Atleta', max_length=1, regex='^[MF]$')
+    
+    pass

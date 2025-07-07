a3a997cd29b885cab5546d5a2bf30063c0fbe5af
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-07 15:46:36 -0300

    Update models.py

--- WORKOUT_API/atleta/models.py
+++ WORKOUT_API/atleta/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from contrib.models import ModelBase
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -7,8 +8,8 @@ class AtletaModel(ModelBase):
     pk_id: Mapped[int] = mapped_column(integer=True, primary_key=True)
     nome: Mapped[str] = mapped_column(str(50), nullable=False)
     cpf: Mapped[str] = mapped_column(str(11), unique=True, nullable=False)  # Unique identifier for the athlete
-    idade: Mapped[int] = mapped_column(nullable=False)
-    peso: Mapped[float] = mapped_column(nullable=False)
-    altura: Mapped[float] = mapped_column(nullable=False)
-    sexo: Mapped[str] = mapped_column(nullable=False)  # 'M' or 'F'
-    
+    idade: Mapped[int] = mapped_column(integer=True, nullable=False)
+    peso: Mapped[float] = mapped_column(float, nullable=False)
+    altura: Mapped[float] = mapped_column(float, nullable=False)
+    sexo: Mapped[str] = mapped_column(str(1), nullable=False)  # 'M' or 'F'
+    created_at: Mapped[datetime] = mapped_column(datetime, nullable=False)

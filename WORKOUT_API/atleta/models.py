49d07e6bccc0b3b37bcb2170d20ed1e76d1b5204
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-07 15:39:29 -0300

    Update models.py

--- WORKOUT_API/atleta/models.py
+++ WORKOUT_API/atleta/models.py
@@ -1,10 +1,14 @@
-class AtletaModel:
-    def __init__(self, id: int, nome: str, idade: int, peso: float, altura: float):
-        self.id = id
-        self.nome = nome
-        self.idade = idade
-        self.peso = peso
-        self.altura = altura
+from contrib.models import ModelBase
+from sqlalchemy.orm import Mapped, mapped_column
 
-    def __repr__(self):
-        return f"AtletaModel(id={self.id}, nome='{self.nome}', idade={self.idade}, peso={self.peso}, altura={self.altura})"
+class AtletaModel(ModelBase):
+    __tablename__ = 'atletas'
+
+    pk_id: Mapped[int] = mapped_column(integer=True, primary_key=True)
+    nome: Mapped[str] = mapped_column(nullable=False)
+    cpf: Mapped[str] = mapped_column(unique=True, nullable=False)  # Unique identifier for the athlete
+    idade: Mapped[int] = mapped_column(nullable=False)
+    peso: Mapped[float] = mapped_column(nullable=False)
+    altura: Mapped[float] = mapped_column(nullable=False)
+    sexo: Mapped[str] = mapped_column(nullable=False)  # 'M' or 'F'
+    

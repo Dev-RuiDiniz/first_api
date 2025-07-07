d42ea0c441918a882121fb5ae1efbe4c8783f151
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-07 15:42:11 -0300

    Update models.py

--- WORKOUT_API/atleta/models.py
+++ WORKOUT_API/atleta/models.py
@@ -6,7 +6,7 @@ class AtletaModel(ModelBase):
 
     pk_id: Mapped[int] = mapped_column(integer=True, primary_key=True)
     nome: Mapped[str] = mapped_column(str(50), nullable=False)
-    cpf: Mapped[str] = mapped_column(unique=True, nullable=False)  # Unique identifier for the athlete
+    cpf: Mapped[str] = mapped_column(str(11), unique=True, nullable=False)  # Unique identifier for the athlete
     idade: Mapped[int] = mapped_column(nullable=False)
     peso: Mapped[float] = mapped_column(nullable=False)
     altura: Mapped[float] = mapped_column(nullable=False)

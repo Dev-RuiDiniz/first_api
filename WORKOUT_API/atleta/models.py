390cb2e82790a9387879e4f4464af24319d9956d
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-08 08:07:40 -0300

    update

--- WORKOUT_API/atleta/models.py
+++ WORKOUT_API/atleta/models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from WORKOUT_API.categorias.models import CategoriaModel
 from contrib.models import ModelBase
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Float, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, Integer, String, Float, DateTime
 
 class AtletaModel(ModelBase):
     __tablename__ = 'atletas'
@@ -14,3 +15,5 @@ class AtletaModel(ModelBase):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)  # 'M' ou 'F'
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
+    categoria_id: Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'), nullable=False)
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atletas')
--- WORKOUT_API/categorias/models.py
+++ WORKOUT_API/categorias/models.py
@@ -1,9 +1,12 @@
+from WORKOUT_API.atleta.models import AtletaModel
 from contrib.models import ModelBase
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import Integer, String
+from typing import List
 
 class CategoriaModel(ModelBase):
     __tablename__ = 'categorias'
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    atleta: Mapped[List['AtletaModel']] = relationship(back_populates='categoria')

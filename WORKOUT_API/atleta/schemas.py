a67d9a42582e28f0adf21d8a84af4aac60100f33
Rui Franscisco de Paula <rui.pdiniz@gmail.com>
2025-07-08 08:28:34 -0300

    update 'Annotated'

--- WORKOUT_API/atleta/schemas.py
+++ WORKOUT_API/atleta/schemas.py
@@ -1,11 +1,11 @@
+from typing import Annotated
 from pydantic import Field
 from contrib.schemas import BaseSchema
 
 class Atleta(BaseSchema):
-    nome: str = Field(description='Nome do Atleta', max_length=100)
-    cpf:  str = Field(description='CPF do Atleta', max_length=11, min_length=11)
-    idade: int = Field(description='Idade do Atleta', ge=0, le=120)
-    peso: float = Field(description='Peso do Atleta', ge=0.0)
-    altura: float = Field(description='Altura do Atleta', ge=0.0)
-    sexo: str = Field(description='Sexo do Atleta', max_length=1, regex='^[MF]$')
-    
+    nome: Annotated[str, Field(description='Nome do Atleta', max_length=100)]
+    cpf: Annotated[str, Field(description='CPF do Atleta', max_length=11, min_length=11)]
+    idade: Annotated[int, Field(description='Idade do Atleta', ge=0, le=120)]
+    peso: Annotated[float, Field(description='Peso do Atleta', ge=0.0)]
+    altura: Annotated[float, Field(description='Altura do Atleta', ge=0.0)]
+    sexo: Annotated[str, Field(description='Sexo do Atleta', max_length=1, regex='^[MF]$')]
--- WORKOUT_API/categorias/schemas.py
+++ WORKOUT_API/categorias/schemas.py
@@ -1,6 +1,6 @@
+from typing import Annotated
 from pydantic import Field
 from contrib.schemas import BaseSchema
 
-
 class Categoria(BaseSchema):
-    nome: str = Field(description='Nome da Categoria', max_length=10)
+    nome: Annotated[str, Field(description='Nome da Categoria', max_length=10)]
